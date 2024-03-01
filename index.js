e096afbdcd81816a8a4696877d851bc444fcb09f
EmilioColds <emiliofriastapia2705@outlook.com>
2024-03-01 15:31:59 -0600

    Created an async function with the questions for the user

--- index.js
+++ index.js
@@ -2,3 +2,29 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes');
 
+async function main() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'textLogo',
+            message: 'Please enter max. 3 characters:',
+            validate: input => input.length <= 3
+        },
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'Please enter a color for your text in keyword or hex format:'
+        },
+        {
+            type: 'list',
+            name: 'shapeLogo',
+            message: 'Please choose one of the folloring shapes:',
+            choices: ['Circle', 'Triangle', 'Square']
+        },
+        {
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Please enter a color for your shape in keyword or hex format:'
+        }
+    ]);
+}
